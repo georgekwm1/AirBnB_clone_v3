fc7fc396323dae24fb8b23760dc52e4bac24343c
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-05-28 08:29:13 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -95,6 +95,16 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
         self.assertNotEqual(inst1.updated_at, inst2.updated_at)
+        
+    def test_myfunction_exists(self):
+        self.assertTrue(hasattr(models.engine.db_storage, 'get'),\
+                         "'get' method is not present in db_storage")
+        self.assertTrue(hasattr(models.engine.db_storage, 'count'),\
+                         "'count' method is not present in db_storage")
+        self.assertTrue(hasattr(models.engine.file_storage, 'get'),\
+                         "'get' method is not present in file_storage")
+        self.assertTrue(hasattr(models.engine.file_storage, 'count'),\
+                         "'count' method is not present in file_storage")
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
